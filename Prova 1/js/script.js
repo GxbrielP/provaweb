188b6bfea25a9a5f48ad5ce0272f787b438aa556
Luigi G. Marchetti <92608635+LuigiMarchetti@users.noreply.github.com>
2023-05-07 22:19:08 -0300

    Add files via upload

--- Prova 1/js/script.js
+++ Prova 1/js/script.js
@@ -1,8 +1,8 @@
 function assignValues(){
-    sessionStorage.setItem("usuario", document.getElementById('usuario').value);
-    sessionStorage.setItem("senha", document.getElementById('senha').value);
-    sessionStorage.setItem("grau", document.getElementById('grau').value);
-    sessionStorage.setItem("data", document.getElementById('data').value);
+    localStorage.setItem("nome", document.getElementById('nome').value);
+    localStorage.setItem("sobrenome", document.getElementById('sobrenome').value);
+    localStorage.setItem("grau", document.getElementById('grau').value);
+    localStorage.setItem("data", document.getElementById('data').value);
 
     let sexo = document.getElementsByName('sexo');
     let sexoValue;
@@ -12,7 +12,7 @@ function assignValues(){
         }
     }
     console.log(sexoValue)
-    sessionStorage.setItem("sexo", sexoValue);
+    localStorage.setItem("sexo", sexoValue);
 
 
     let estado = document.getElementsByName('estado');
@@ -23,6 +23,67 @@ function assignValues(){
         }
     }
     console.log(estadoValue)
-    sessionStorage.setItem("estado", estadoValue);
+    localStorage.setItem("estado", estadoValue);
 
+}
+
+function onLoad() {
+    const username = document.getElementById('username')
+    let usuario = localStorage.getItem('usuario');
+    if(usuario){
+        username.innerText = usuario;
+    }
+    
+    if (localStorage.getItem('colorDiv')){
+        loadCustom();
+        document.getElementById('pgcustom').checked = true;
+    }
+}
+
+function loadCustom(){
+    divs = document.getElementsByTagName('div');
+    ps = document.getElementsByTagName('p');
+
+    for (i = 0; i < divs.length; i++) {
+        divs[i].style = 'background-color: ' + localStorage.getItem('colorDiv');
+    }
+
+    for (i = 0; i < ps.length; i++) {
+        ps[i].style = 'background-color: ' + localStorage.getItem('colorDiv');
+    }
+    document.body.style = 'background-color: ' + localStorage.getItem('colorBody') + '; font-family: ' + localStorage.getItem('font');
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    onLoad();
+});
+
+function register(){
+    usuario = document.getElementById('usuario').value;
+    senha = document.getElementById('senha').value;
+    if (usuario === '' || senha === ''){
+        alert("Informe os dados de autenticação");
+    }
+    else{
+        localStorage.setItem("usuario", usuario);
+        localStorage.setItem("senha", senha);
+        window.location.href = "index.html";
+    }
+}
+
+
+
+function deleteCustom(){
+    localStorage.removeItem('colorDiv');
+    localStorage.removeItem('font');
+    localStorage.removeItem('colorBody');
+    location.reload();
+}
+
+function createCustom(){
+    localStorage.setItem("colorDiv", "white");
+    localStorage.setItem("font", "Helvetica");
+    localStorage.setItem("colorBody", 'white');
+    loadCustom();
 }
